b3d53aef62d63ca63c425128a4284c19864b3569
Nicko15 <84836228+Nicko15@users.noreply.github.com>
2021-06-14 13:52:02 +0530

    Update Colors.js

--- Colors.js
+++ Colors.js
@@ -6,12 +6,10 @@ for(var i=0;i<x.length;i++) {
 
 		if(x[i].tagName[0] == "C" && x[i].tagName[1] == "_") {
 
-		if(x[i].style.color == "") {
-
  x[i].style.color = x[i].tagName.replace("C_","#");
 
-	  }
+	  
+    }
   }
-}
 }
 colors();
